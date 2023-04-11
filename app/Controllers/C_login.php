247376af0a439fa33fae03d0a52c68794f5a8395
Algi028 <giwalgi028@gmail.com>
2023-04-11 14:57:07 +0700

    controll dashboard

---
+++ app/Controllers/C_login.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class C_login extends BaseController
+{
+    
+    public function index()
+    {
+        return view('v_login');
+    }
+
+    
+
+    
+
+   
+}
--- app/Views/v_login.php
+++ app/Views/v_login.php
@@ -25,9 +25,9 @@
     <div class="card-body login-card-body">
       <p class="login-box-msg">Masuk untuk memulai sesi</p>
 
-      <form action="<?= base_url()?>home/proses_login" method="post">
+      <form action="<?= base_url()?>login/proses_login" method="post">
         <div class="input-group mb-3">
-          <input type="name" name="username" class="form-control" placeholder="Nama" required>
+          <input type="name" id="username" name="username" class="form-control" placeholder="Nama" required>
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
@@ -35,7 +35,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="email" name="email" class="form-control" placeholder="Email" required>
+          <input type="email" id="email" name="email" class="form-control" placeholder="Email" required>
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -43,24 +43,15 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Password" required>
+          <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
           <!-- /.col -->
-          <div class="col-4">
+          <div class="col-4" center>
             <button type="submit" class="btn btn-primary btn-block">LOGIN</button>
           </div>
           <!-- /.col -->
